357ceb01cb7eae0a81e1c4004e624f7849963404
sridnala <43190526+sridnala@users.noreply.github.com>
2018-10-12 22:47:52 -0400

    Moved methods to util.py

--- src/msds510/util.py
+++ src/msds510/util.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 from datetime import date
 
 
@@ -29,3 +30,41 @@ def days_since_joined(yearValue, introValue):
         print("Unable to count total number of days")
         return None
     return totalDays
+
+
+def getFileNameAndPath(filePath):  # To split folder path and file name from argument
+    try:
+        index = filePath.rindex("/")
+        inputFolderPath = filePath[0:index]
+        inputFileName = filePath[index:]
+        return inputFileName,inputFolderPath
+    except BaseException as e:
+        print("Exception in getFileNameAndPath function")
+        print(str(e))
+
+
+def verifyArguments(argv):
+    try:
+        argumentsCount = len(argv)
+        if argumentsCount == 3:  # To verify number of command line arguments
+            if argv[1] == argv[2]:  # To verify source and target file paths
+                print("Input and output files are same. Please check file name(s) and Path")
+                return False
+            else:
+                inputFileName, inputFolderPath = getFileNameAndPath(argv[1])
+                outputFileName, outputFolderPath = getFileNameAndPath(argv[2])
+                if inputFileName and outputFileName:  # To verify file names are not empty
+                    if not os.path.exists(outputFolderPath):
+                        print("Creating directory")
+                        os.mkdir(outputFolderPath)      # Create output directory if not exist
+                    return True
+                else:
+                    print("Invalid File Names")
+                    return False
+        else:
+            print("Invalid Number of Arguments")
+            return False
+    except BaseException  as e:
+        print("Exception in verifyArguments function")
+        print(str(e))
+        return False
